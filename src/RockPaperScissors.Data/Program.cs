4c5912bf7d360d96d76d90547b11c8f6314cc7ce
Connor Stewart <connor.stewart1224@gmail.com>
2022-06-18 15:00:30 -0400

    Working game

---
+++ src/RockPaperScissors.Data/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+public class RockPaperScissors
+{
+    public string userInput { get; set; }
+    public int computerInput { get; set; }
+    public int inputID {get; set;}
+    
+
+
+    // public string Victory
+    //     {
+    //     // Automatically setting a color value to the card depending on the Suit assigned.
+    //         get
+    //         {
+    //             if(computerInput == RPS.Rock && userInput == RPS.Rock)
+    //             {
+    //                 return "Tie";
+    //             }
+    //             else if(computerInput == RPS.Rock && userInput == RPS.Paper)
+    //             {
+    //                 return "win";
+    //             }
+    //             else if(computerInput == RPS.Rock && userInput == RPS.Scissors)
+    //             {
+    //                 return "loss";
+    //             }
+    //             else if(computerInput == RPS.Paper && userInput == RPS.Rock)
+    //             {
+    //                 return "loss";
+    //             }
+    //             else if(computerInput == RPS.Paper && userInput == RPS.Paper)
+    //             {
+    //                 return "tie";
+    //             }
+    //             else if(computerInput == RPS.Paper && userInput == RPS.Scissors)
+    //             {
+    //                 return "win";
+    //             }
+    //             else if(computerInput == RPS.Scissors && userInput == RPS.Rock)
+    //             {
+    //                 return "win";
+    //             }
+    //             else if(computerInput == RPS.Scissors && userInput == RPS.Paper)
+    //             {
+    //                 return "loss";
+    //             }
+    //             else if(computerInput == RPS.Scissors && userInput == RPS.Scissors)
+    //             {
+    //                 return "tie";
+    //             }
+    //             else
+    //             {
+    //                 return "Invalid entry.";
+    //             }
+    //         }
+    //     }
+    
+}
